9eb6e970f91faebc17d744df010d78bffb2ca22f
tim <timgarciaa@gmail.com>
2023-06-13 16:45:52 +0800

    Working with components basic

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,7 +1,16 @@
+import Expenses from "./components/Expenses";
+
 function App() {
+  // const para = document.createElement('p');
+  // para.textContent = 'This is also visible 2';
+  // document.getElementById('root').append(para);
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
+
+      <Expenses />
     </div>
   );
 }
