dba5bb528190e3aa4a3c99bedf93d4826dd6bdee
tim <timgarciaa@gmail.com>
2023-06-14 15:59:37 +0800

    Practice component basics

---
+++ 02-starting-setup/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Concepts from "./components/Concepts";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Concepts />
+    </div>
+  );
+}
+
+export default App;
---
+++ 02-starting-setup/src/components/ConceptCard.js
@@ -0,0 +1,11 @@
+function ConceptCard({concept}) {
+  return (
+    <li className="concept">
+      <img src={concept.image} alt={concept.title} />
+      <h2>{concept.title}</h2>
+      <p>{concept.description}</p>
+    </li>
+  );
+}
+
+export default ConceptCard;
